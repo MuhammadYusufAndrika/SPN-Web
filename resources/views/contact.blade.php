87293fbe0a284fa893c9540a76985dcec2ece46a
rhnrdnsyh <asusa516jao.2323@outlook.com>
2024-06-04 20:56:03 +0700

    nambah contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,273 @@
+<!-- Ini aku yang ngerjain (Rehan) -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact SPN - Surya Prima Net</title>
+    <link rel="shortcut icon" href="/assets/images/SPN.png" type="image/x-icon" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="sass/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+    <script src="https://unpkg.com/feather-icons"></script>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand me-auto" href="#">
+        <img
+          src="/assets/images/banner.jpg"
+          alt="SPN"
+          width="100"
+          height="auto"
+        />
+      </a>
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasNavbar"
+        aria-labelledby="offcanvasNavbarLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasNavbarLabel">SPN</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+          <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/Index.html">Home</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Layanan
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/payment.html">Internet</a></li>
+                <li>
+                  <a class="dropdown-item" href="jarkomlayanan.html">Jaringan Komputer</a>
+                </li>
+                <li><a class="dropdown-item" href="/cctvlayanan.html">CCTV</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/about.html">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/contact.html">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <a href="/profile.html" class="login"><i data-feather="user"></i></a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasNavbar"
+        aria-controls="offcanvasNavbar"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+  </nav>
+<!-- carousel start -->
+
+      <div id="carouselExampleIndicators" class="carousel slide">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner position-relative" style="width: 100%; height: 75vh">
+          <div class="carousel-item active">
+            <img src="assets/images/carousel-1 (1).jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block " style="height: 60%;">
+              <h1 style="font-weight: 700;">Membawa Dunia Menjadi Lebih Dekat</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="assets/images/carousel-2.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block " style="height: 60%;">
+              <h1 style="font-weight: 700;">Membawa Dunia Menjadi Lebih Dekat</h1>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="assets/images/carousel-3.jpg" class="d-block w-100" alt="..." />
+            <div class="carousel-caption d-none d-md-block " style="height: 60%;">
+              <h1 style="font-weight: 700;">Membawa Dunia Menjadi Lebih Dekat</h1>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+<!-- carousel end -->
+
+    <div class="judul">
+      <h2>
+        <span class="kantor">Kantor</span> 
+        <span class="kami">Kami</span>
+      </h2>
+    </div>
+  <section class="peta">
+    <div class="card w-50 p-3" >
+      <div class="ratio ratio-4x3">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d986.4952580892519!2d116.66311432851101!3d-8.501221973099224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcc393b29ac144d%3A0xb0622dfdcbe4238f!2sJSN%20-%20Kantor%20Layanan%20Labuhan%20Lombok!5e0!3m2!1sid!2sid!4v1715173013393!5m2!1sid!2sid" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+    </div> 
+      <div class="reportbox col-md-6">
+        <h2 class="reporttext text-uppercase mt-3 font-weight-bold text-black">Pengaduan Masalah</h2>
+        <form action="">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="form-group">
+                <input type="text" class="form-control mt-2" placeholder="Nama" required>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="form-group">
+                <input type="text" class="form-control mt-2" placeholder="Email" required>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="form-group">
+                <input type="email" class="form-control mt-2" placeholder="Alamat" required>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="form-group">
+                <input type="text" class="form-control mt-2" placeholder="No. HP" required>
+              </div>
+            </div>
+            <div class="col-12">
+              <div class="form-group">
+                <textarea class="form-control mt-2" id="exampleFormControlTextarea1" placeholder="Deskribsi Keluhan" rows="3" required></textarea>
+              </div>
+            </div>
+            <div class="col-12">
+            <div class="form-group">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                <label class="form-check-label" for="invalidCheck2" style="color: black; font-weight: 100;">
+                  Saya dapat memastikan bahwa data di atas benar
+                </label>
+              </div>
+            </div>
+            </div>
+            <div class=" tombolkirim col-12">
+              <button class="  btn btn-light" type="submit">Kirim</button>
+            </div>
+          </div>
+          </form>
+          <div class="textinfo text-black">
+              <h2 class="text-uppercase mt-4 font-weight-bold">Admin Kami</h2>
+              <i class="fas fa-phone mt-3"></i> 082144299555<br>
+              <i class="fa fa-envelope mt-3"></i> <a href="">azka@gmail.com</a><br>
+              <i class="fas fa-globe mt-3"></i> Jln. Raya Labuan Lombok<br>
+
+            </div>
+      </div>
+  </section>
+
+  <div class="penutup">
+    <footer class="text-center text-lg-start text-dark" style="background-color:rgba(0, 0, 0, 0.2)">
+      <!-- Section: Social media -->
+      <section class="d-flex justify-content-between p-4 text-white" style="background-color: #3259ab; height: 80px;">
+        <!-- Left -->
+
+        <!-- Left -->
+
+        <!-- Right -->
+        <!-- Right -->
+      </section>
+      <!-- Section: Social media -->
+
+      <!-- Section: Links  -->
+      <section class="" style="height: 300px;">
+        <div class="container text-center text-md-start mt-5" style="height: 200px;">
+          <!-- Grid row -->
+          <div class="row mt-3">
+            <!-- Grid column -->
+            <div class="bebas col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+              <!-- Content -->
+              <h6 class="text-uppercase fw-bold" style="margin-top: 0%;">Surya Prima Net</h6>
+              <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px" />
+              <p>Kami tau anda pasti membutuhkan layanan yang kami sediakan, maka dari itu kamu menyediakan layanan yang
+                cocok buat anda</p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <!-- Links -->
+              <h6 class="text-uppercase fw-bold">Products</h6>
+              <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px" />
+              <p>
+                <a href="#!" class="text-dark">Internet</a>
+              </p>
+              <p>
+                <a href="#!" class="text-dark">Jaringan komputer</a>
+              </p>
+              <p>
+                <a href="#!" class="text-dark">CCTV</a>
+              </p>
+            </div>
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold">Useful links</h6>
+              <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px" />
+              <p>
+                <a href="#!" class="text-dark">Home</a>
+              </p>
+              <p>
+                <a href="#!" class="text-dark">About</a>
+              </p>
+              <p>
+                <a href="#!" class="text-dark">Contact</a>
+              </p>
+            </div>
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+              <h6 class="text-uppercase fw-bold">Contact</h6>
+              <hr class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 60px; background-color: #7c4dff; height: 2px" />
+              <p><i class="fas fa-home mr-3"></i> Jl. Raya Labuhan Lombok</p>
+              <p>
+                <i class="fas fa-envelope mr-3"></i>
+                SuryaPrimaNet@gmail.com
+              </p>
+              <p><i class="fas fa-phone mr-3"></i>08++++++++++</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2024 Copyright:
+        <a class="text-dark" href="#">SPN - Surya Prima Net</a>
+      </div>
+    </div>
+     
+</body>
+<script> feather.replace();</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</html>
