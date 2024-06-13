ad3a1a356f9623200577580225a87f0102dc8518
rhnrdnsyh <asusa516jao.2323@outlook.com>
2024-06-13 10:59:16 +0700

    nambah live chat di contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,6 +13,17 @@
     <script src="https://unpkg.com/feather-icons"></script>
 </head>
 <body>
+  <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6666c6ec981b6c56477b7f0a/1i00ni3l6';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+  </script>
   <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
       <a class="navbar-brand me-auto" href="#">
