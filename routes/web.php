d9cf64c688dfd291124145b13b8ab486eb79949a
Khoirul <MSI@ADMINISTRATOR>
2024-06-30 08:50:07 +0700

    fix profile header, routing

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,7 @@ use App\Models\LayananInternet;
 use App\Models\LayananKomputer;
 use App\Models\Pengaduan;
 
-
-
-Route::get('/', [IndexController::class, 'index']);
+Route::get('/', [IndexController::class, 'index'])->name('index');
 
 Route::prefix('admin')->name('admin.')->group(function () {
     // Rute untuk tampilan admin.blade.php
@@ -33,36 +31,35 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('contact', PengaduanController::class);
 });
 
-// Route::prefix('admin')->name('admin.')->group(function () {
-//     Route::resource('carousels', CarouselController::class);
-//     Route::resource('descriptions', DescriptionController::class);
-//     Route::resource('services', ServiceController::class);
-//     Route::resource('layanan-internet', LayananInternetController::class);
-// });
-
-// Corrected syntax and added route name
+// Mengatur route untuk URL '/profile'
 Route::get('/profile', function () {
+    // Mengembalikan view bernama 'profile'
     return view('profile');
 })->name('profile');
 
+// Mengatur route untuk URL '/contact'
 Route::get('/contact', function () {
     return view('contact');
 })->name('contact');
 
+
+// Mengatur route untuk URL '/cctvlayanan'
 Route::get('/cctvlayanan', function () {
+    // ambil semua data dari model LayananCCTV simpan ke variabel $sections
     $sections = LayananCCTV::all();
     return view('cctvlayanan', compact('sections'));
 })->name('cctvlayanan');
 
+// Mengatur route untuk URL '/komputerlayanan'
 Route::get('/komputerlayanan', function () {
     $sections = LayananKomputer::all();
     return view('komputerlayanan', compact('sections'));
 })->name('komputerlayanan');
 
+// Mengatur route untuk URL '/internetlayanan'
 Route::get('/internetlayanan', function () {
     $sections = LayananInternet::all();
     return view('internetlayanan', compact('sections'));
 })->name('internetlayanan');
 
 Route::post('create-contact', [PengaduanController::class, "store"]);
-// Route::resource('layanan-jarkom', [PengaduanController::class, "store"]);
